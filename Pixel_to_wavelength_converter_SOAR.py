a7c735f4417e560c4a5baaa3f430b69cff5cbd05
Bebi Muskan Rai <bebirai2023@gmail.com>
2026-01-13 19:10:06 -0600

    Update Pixel_to_wavelength_converter_SOAR.py

--- Pixel_to_wavelength_converter_SOAR.py
+++ Pixel_to_wavelength_converter_SOAR.py
@@ -10,19 +10,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 from astropy.io import fits
 def pixel_to_lambda(x):
-    slope = 1.428038473597098 
-    intercept = 2869.633658089779
+    slope = 'Enter the slope'
+    intercept = 'Enter the intercept'
     return slope*x+intercept
 
 # Replace with your actual .fits file path
-fits_file_path = 'wecfzst_0232_178822nd_23-12-2024_445_ws_1.fits'
+fits_file_path = 'File_name.fits'
 
 # Open the .fits file
 with fits.open(fits_file_path) as hdul:
     data = hdul[0].data
-
-# Check the shape of the data
-print(f"Data shape: {data.shape}")
 
 spectrum_width_pixels = len(data)
 pixel_values = np.arange(start=0,stop=spectrum_width_pixels,step=1)
@@ -37,4 +34,4 @@ plt.title('FITS Data Line Plot')
 plt.xlabel('Index')
 plt.ylabel('Value')
 plt.show()
-plt.savefig('wecfzst_0232_178822nd_23-12-2024_445_ws_1.png')
+plt.savefig('Figure.png')
